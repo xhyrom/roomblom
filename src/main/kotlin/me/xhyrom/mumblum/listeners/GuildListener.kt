0ef3b7e1e1c02d56287d349ea74ff6fa5fb9af2e
xhyrom <generalkubo@gmail.com>
2022-12-18 11:12:31 +0100

    fix: destroy music manager when disconnect

--- src/main/kotlin/me/xhyrom/mumblum/listeners/GuildListener.kt
+++ src/main/kotlin/me/xhyrom/mumblum/listeners/GuildListener.kt
@@ -7,8 +7,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 class GuildListener : ListenerAdapter() {
     override fun onGuildVoiceUpdate(event: GuildVoiceUpdateEvent) {
         if (event.channelLeft != null) {
-            if (!event.oldValue!!.members.contains(event.guild.selfMember)) return
-
             if (!event.channelLeft!!.members.contains(event.guild.selfMember)) {
                 val musicManager = Bot.getInstanceUnsafe().getLavaLinkManager().getGuildMusicManager(event.guild)
                 musicManager.getLink().destroy()
