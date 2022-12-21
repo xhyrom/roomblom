605253c52adbf304bf049d92552297cbab252b13
xhyrom <generalkubo@gmail.com>
2022-12-21 20:30:19 +0100

    feat: implement more sources

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,15 +14,18 @@ repositories {
     maven("https://m2.dv8tion.net/releases")
     maven("https://jitpack.io")
     maven("https://m2.duncte123.dev/releases")
+    maven("https://repo.jopga.me/releases")
 }
 
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.2")
-    implementation("com.github.walkyst:lavaplayer-fork:1.3.98.4")
     //implementation("com.sedmelluq:lavaplayer:1.3.78")
     //implementation("com.github.jagrosh:lavaplayer:jmusicbot-SNAPSHOT")
     //implementation("com.github.freyacodes:Lavalink-Client:cde746afc")
-    implementation(files("libs/Lavalink-Client.jar"))
+    //implementation(files("libs/Lavalink-Client.jar"))
+    implementation("com.fredboat.lavalink.client:Lavalink-Client:ea092bd6410752a95e9460affc844015e73a904b-SNAPSHOT")
+    implementation("com.github.walkyst:lavaplayer-fork:1.3.98.4")
+
     implementation("com.github.the-codeboy:Piston4J:0.0.7")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
     implementation("com.dunctebot:sourcemanagers:1.8.2")
